fae6b54e8d3edf8c890ca3be212b27d591acad03
Andre Eisenbach <eisenbach@google.com>
2016-03-21 10:15:30 -0700

    Make sure wakelock is held before releaseing it
    Bug: 27748109
    Change-Id: I38c8ec640f5def7c53348d2dd96a0a2492ff00cc

--- src/com/android/bluetooth/btservice/AdapterService.java
+++ src/com/android/bluetooth/btservice/AdapterService.java
@@ -547,7 +547,8 @@ public class AdapterService extends Service {
         // {@link #releaseWakeLock(String lockName)}, so a synchronization is needed here.
         synchronized (this) {
             if (mWakeLock != null) {
-                mWakeLock.release();
+                if (mWakeLock.isHeld())
+                    mWakeLock.release();
                 mWakeLock = null;
             }
         }
@@ -2020,14 +2021,15 @@ public class AdapterService extends Service {
     // extended to allow acquiring an arbitrary number of wake locks. The current interface
     // takes |lockName| as a parameter in anticipation of that implementation.
     private boolean acquireWakeLock(String lockName) {
-        if (mWakeLock == null) {
-            mWakeLockName = lockName;
-            mWakeLock = mPowerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, lockName);
+        synchronized (this) {
+            if (mWakeLock == null) {
+                mWakeLockName = lockName;
+                mWakeLock = mPowerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, lockName);
+            }
+
+            if (!mWakeLock.isHeld())
+                mWakeLock.acquire();
         }
-
-        if (!mWakeLock.isHeld())
-            mWakeLock.acquire();
-
         return true;
     }
 
@@ -2042,7 +2044,8 @@ public class AdapterService extends Service {
                 return false;
             }
 
-            mWakeLock.release();
+            if (mWakeLock.isHeld())
+                mWakeLock.release();
         }
         return true;
     }
