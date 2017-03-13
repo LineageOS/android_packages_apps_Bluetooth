0a1cdd5444b9b196646640a70dc9b6099b0dbee2
Jack He <siyuanh@google.com>
2017-03-13 10:37:06 -0700

    OPP: Restrict file based URI access to external storage
    * Allow only external storage paths in file based URI in
      BluetoothOppSendFileInfo when the file send request comes from an
      external source
    * Fix a potential NPE when using Uri.getPath()
    
    Bug: 35310991
    Test: Make, test various cases of Bluetooth file share
    Change-Id: Id5519588266f22674b6d01ec6ebe34fe38cbccce
    (cherry picked from commit 3edd7f0a8aadf2f44bc62ea5b567c74d39a534c8)
    (cherry picked from commit 3ce229fa602f739d1e98e2a85a3bd955c3a308f6)

--- src/com/android/bluetooth/opp/BluetoothOppManager.java
+++ src/com/android/bluetooth/opp/BluetoothOppManager.java
@@ -251,34 +251,31 @@ public class BluetoothOppManager {
         if (V) Log.v(TAG, "Application data stored to SharedPreference! ");
     }
 
-    public void saveSendingFileInfo(String mimeType, String uriString, boolean isHandover) {
+    public void saveSendingFileInfo(String mimeType, String uriString, boolean isHandover,
+            boolean fromExternal) {
         synchronized (BluetoothOppManager.this) {
             mMultipleFlag = false;
             mMimeTypeOfSendingFile = mimeType;
             mIsHandoverInitiated = isHandover;
             Uri uri = Uri.parse(uriString);
-            BluetoothOppSendFileInfo sendFileInfo =
-                BluetoothOppSendFileInfo.generateFileInfo(mContext, uri, mimeType);
-            uri = BluetoothOppUtility.generateUri(uri, sendFileInfo);
-            BluetoothOppUtility.putSendFileInfo(uri, sendFileInfo);
-            mUriOfSendingFile = uri.toString();
-            mNameOfSendingFile = sendFileInfo.mFileName;
+            BluetoothOppUtility.putSendFileInfo(
+                    uri, BluetoothOppSendFileInfo.generateFileInfo(
+                                 mContext, uri, mimeType, fromExternal));
             storeApplicationData();
         }
     }
 
-    public void saveSendingFileInfo(String mimeType, ArrayList<Uri> uris, boolean isHandover) {
+    public void saveSendingFileInfo(String mimeType, ArrayList<Uri> uris, boolean isHandover,
+            boolean fromExternal) {
         synchronized (BluetoothOppManager.this) {
             mMultipleFlag = true;
             mMimeTypeOfSendingFiles = mimeType;
             mUrisOfSendingFiles = new ArrayList<Uri>();
             mIsHandoverInitiated = isHandover;
             for (Uri uri : uris) {
-                BluetoothOppSendFileInfo sendFileInfo =
-                    BluetoothOppSendFileInfo.generateFileInfo(mContext, uri, mimeType);
-                uri = BluetoothOppUtility.generateUri(uri, sendFileInfo);
-                mUrisOfSendingFiles.add(uri);
-                BluetoothOppUtility.putSendFileInfo(uri, sendFileInfo);
+                BluetoothOppUtility.putSendFileInfo(
+                        uri, BluetoothOppSendFileInfo.generateFileInfo(
+                                     mContext, uri, mimeType, fromExternal));
             }
             storeApplicationData();
         }
