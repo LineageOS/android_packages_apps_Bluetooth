a7f8f1632728d511d14cc9f971afdf8a155b66f4
Dan Pasanen <dan.pasanen@gmail.com>
2018-01-09 08:14:27 -0600

    Avrcp: disable debugging
    * Please stop spamming my logs. Thanks.
    
    Change-Id: I292160c2fe147c193df20bd960ef072a76ddc0a8

--- src/com/android/bluetooth/avrcp/Avrcp.java
+++ src/com/android/bluetooth/avrcp/Avrcp.java
@@ -72,7 +72,7 @@ import java.util.TreeMap;
  ******************************************************************************/
 
 public final class Avrcp {
-    private static final boolean DEBUG = true;
+    private static final boolean DEBUG = false;
     private static final String TAG = "Avrcp";
     private static final String ABSOLUTE_VOLUME_BLACKLIST = "absolute_volume_blacklist";
 
