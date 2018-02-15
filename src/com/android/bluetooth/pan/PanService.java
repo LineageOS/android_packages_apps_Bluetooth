codeaurora-unoffical/platform-packages-apps-Bluetooth

PAN: Notify PAN devices connection state to UI
Usecase:
1) Try to connect to remote device (where tethering is disabled)
2) Perform BT on off whiel above is ongoing

Expected Beahviour
UI shows correct status as connected/disconnected based on device is connected or
disconnected with remote device

Observed Result
DUT UI shows connected, even when remote device is not connected with DUT

Root Cause:
BluetoothPan.ACTION_CONNECTION_STATE_CHANGED is not broadcasted when BT is turned off
while Pan state is Connecting

Fix:
1) Notify PAN devices connection state to UI during BT turn off.
2) Update proper role during handlePanDeviceStateChange so as to stop Tethering or
   Reverse Tethering as the case may be during BT off time if enabled.

Test: UI is informed of PAN connection status during BT turning off state as well

Fixes: 73018139

CRs-fixed: 2184657
Change-Id: I4b1413d68418c6675b8638d3f14e04aece84d3d4

src/com/android/bluetooth/pan/PanService.java
@@ -156,10 +156,16 @@ public class PanService extends ProfileService {
             mNativeAvailable=false;
         }
         if(mPanDevices != null) {
-            List<BluetoothDevice> DevList = getConnectedDevices();
-            for(BluetoothDevice dev : DevList) {
-                handlePanDeviceStateChange(dev, mPanIfName, BluetoothProfile.STATE_DISCONNECTED,
-                        BluetoothPan.LOCAL_PANU_ROLE, BluetoothPan.REMOTE_NAP_ROLE);
+           for (BluetoothDevice device: mPanDevices.keySet()) {
+                BluetoothPanDevice panDevice = mPanDevices.get(device);
+                Log.d(TAG, "panDevice: " + panDevice + " device address: " + device);
+                if (panDevice != null
+                    && panDevice.mState != BluetoothProfile.STATE_DISCONNECTED) {
+                    handlePanDeviceStateChange(device, mPanIfName,
+                        BluetoothProfile.STATE_DISCONNECTED,
+                        panDevice.mLocalRole, panDevice.mRemoteRole);
+                        break;
+                }
             }
             mPanDevices.clear();
         }
@@ -480,12 +486,13 @@ public class PanService extends ProfileService {
         if (panDevice == null) {
             Log.i(TAG, "state " + state + " Num of connected pan devices: " + mPanDevices.size());
             prevState = BluetoothProfile.STATE_DISCONNECTED;
-            panDevice = new BluetoothPanDevice(state, iface, local_role);
+            panDevice = new BluetoothPanDevice(state, iface, local_role, remote_role);
             mPanDevices.put(device, panDevice);
         } else {
             prevState = panDevice.mState;
             panDevice.mState = state;
             panDevice.mLocalRole = local_role;
+            panDevice.mRemoteRole = remote_role;
             panDevice.mIface = iface;
         }
 
@@ -672,11 +679,13 @@ public class PanService extends ProfileService {
         private int mState;
         private String mIface;
         private int mLocalRole; // Which local role is this PAN device bound to
+        private int mRemoteRole; // Which remote role is this PAN device bound to
 
-        BluetoothPanDevice(int state, String iface, int localRole) {
+        BluetoothPanDevice(int state, String iface, int localRole, int remoteRole) {
             mState = state;
             mIface = iface;
             mLocalRole = localRole;
+            mRemoteRole = remoteRole;
         }
     }
 

LineageOS/android_packages_apps_Bluetooth

src/com/android/bluetooth/pan/PanService.java
@@ -137,10 +137,18 @@ public class PanService extends ProfileService {
             mNativeAvailable = false;
         }
         if (mPanDevices != null) {
-            List<BluetoothDevice> devList = getConnectedDevices();
-            for (BluetoothDevice dev : devList) {
-                handlePanDeviceStateChange(dev, mPanIfName, BluetoothProfile.STATE_DISCONNECTED,
-                        BluetoothPan.LOCAL_PANU_ROLE, BluetoothPan.REMOTE_NAP_ROLE);
+           int[] desiredStates = {BluetoothProfile.STATE_CONNECTING, BluetoothProfile.STATE_CONNECTED,
+                                  BluetoothProfile.STATE_DISCONNECTING};
+           List<BluetoothDevice> devList =
+                   getDevicesMatchingConnectionStates(desiredStates);
+           for (BluetoothDevice device : devList) {
+                BluetoothPanDevice panDevice = mPanDevices.get(device);
+                Log.d(TAG, "panDevice: " + panDevice + " device address: " + device);
+                if (panDevice != null) {
+                    handlePanDeviceStateChange(device, mPanIfName,
+                        BluetoothProfile.STATE_DISCONNECTED,
+                        panDevice.mLocalRole, panDevice.mRemoteRole);
+                }
             }
             mPanDevices.clear();
         }
@@ -497,12 +505,13 @@ public class PanService extends ProfileService {
         if (panDevice == null) {
             Log.i(TAG, "state " + state + " Num of connected pan devices: " + mPanDevices.size());
             prevState = BluetoothProfile.STATE_DISCONNECTED;
-            panDevice = new BluetoothPanDevice(state, iface, localRole);
+            panDevice = new BluetoothPanDevice(state, iface, localRole, remoteRole);
             mPanDevices.put(device, panDevice);
         } else {
             prevState = panDevice.mState;
             panDevice.mState = state;
             panDevice.mLocalRole = localRole;
+            panDevice.mRemoteRole = remoteRole;
             panDevice.mIface = iface;
         }
 
@@ -689,11 +698,13 @@ public class PanService extends ProfileService {
         private int mState;
         private String mIface;
         private int mLocalRole; // Which local role is this PAN device bound to
+        private int mRemoteRole; // Which remote role is this PAN device bound to
 
-        BluetoothPanDevice(int state, String iface, int localRole) {
+        BluetoothPanDevice(int state, String iface, int localRole, int remoteRole) {
             mState = state;
             mIface = iface;
             mLocalRole = localRole;
+            mRemoteRole = remoteRole;
         }
     }
 