f37b51f4b4d6d7259898fe71598dd1994be71f31
Ajay Panicker <apanicke@google.com>
2018-03-16 17:34:14 -0700

    Add a property to enable the New AVRCP Profile (2/2)
    You can now use the new AVRCP Profile by setting
    persist.bluetooth.enablenewavrcp to true. Also add a binder interface
    that will be used later in the future.
    
    Bug: 68854188
    Test: Check to see that the profile is used when property is true and is
          not used when property is false.
    
    Change-Id: Ia68696cfcb1c8077ab80cfe90f345bd0a09e185f

--- src/com/android/bluetooth/a2dp/A2dpService.java
+++ src/com/android/bluetooth/a2dp/A2dpService.java
@@ -37,6 +37,7 @@ import android.util.Log;
 
 import com.android.bluetooth.Utils;
 import com.android.bluetooth.avrcp.Avrcp;
+import com.android.bluetooth.avrcp.AvrcpTargetService;
 import com.android.bluetooth.btservice.AdapterService;
 import com.android.bluetooth.btservice.ProfileService;
 
@@ -531,6 +532,13 @@ public class A2dpService extends ProfileService {
     }
 
     public void setAvrcpAbsoluteVolume(int volume) {
+        // TODO (apanicke): Instead of using A2DP as a middleman for volume changes, add a binder
+        // service to the new AVRCP Profile and have the audio manager use that instead.
+        if (AvrcpTargetService.get() != null) {
+            AvrcpTargetService.get().sendVolumeChanged(volume);
+            return;
+        }
+
         mAvrcp.setAbsoluteVolume(volume);
     }
 
