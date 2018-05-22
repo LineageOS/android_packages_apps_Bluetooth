099b9449d5c11e26f5dc17bd14170f3fe0a2e412
Pavlin Radoslavov <pavlin@google.com>
2018-05-22 04:09:52 -0700

    Allow outgoing A2DP connection when pairing via NFC
    If Bluetooth is disabled, pairing via NFC will start the Bluetooth
    Adapter service in Quiet Mode. In such mode only the incoming A2DP
    connection requests should be rejected - the outgoing A2DP connection
    requests should be allowed.
    
    Bug: 79330878
    Test: Manual: (1) Disable Bluetooth; (2) Pair with a Headset via NFC;
          (3) Play music. Updated existing unit tests.
    
    Change-Id: I3dd892b963116c8f169f63bb117401a4a078ab9c
    Merged-In: I3dd892b963116c8f169f63bb117401a4a078ab9c
    (cherry picked from commit c713e54b82851072afdc5fb6bc6decc2f438d81b)

--- src/com/android/bluetooth/a2dp/A2dpStateMachine.java
+++ src/com/android/bluetooth/a2dp/A2dpStateMachine.java
@@ -185,7 +185,7 @@ final class A2dpStateMachine extends StateMachine {
                         Log.e(TAG, "Disconnected: error connecting to " + mDevice);
                         break;
                     }
-                    if (mA2dpService.okToConnect(mDevice)) {
+                    if (mA2dpService.okToConnect(mDevice, true)) {
                         transitionTo(mConnecting);
                     } else {
                         // Reject the request and stay in Disconnected state
@@ -226,7 +226,7 @@ final class A2dpStateMachine extends StateMachine {
                     Log.w(TAG, "Ignore A2DP DISCONNECTED event: " + mDevice);
                     break;
                 case A2dpStackEvent.CONNECTION_STATE_CONNECTING:
-                    if (mA2dpService.okToConnect(mDevice)) {
+                    if (mA2dpService.okToConnect(mDevice, false)) {
                         Log.i(TAG, "Incoming A2DP Connecting request accepted: " + mDevice);
                         transitionTo(mConnecting);
                     } else {
@@ -237,7 +237,7 @@ final class A2dpStateMachine extends StateMachine {
                     break;
                 case A2dpStackEvent.CONNECTION_STATE_CONNECTED:
                     Log.w(TAG, "A2DP Connected from Disconnected state: " + mDevice);
-                    if (mA2dpService.okToConnect(mDevice)) {
+                    if (mA2dpService.okToConnect(mDevice, false)) {
                         Log.i(TAG, "Incoming A2DP Connected request accepted: " + mDevice);
                         transitionTo(mConnected);
                     } else {
@@ -428,7 +428,7 @@ final class A2dpStateMachine extends StateMachine {
                     transitionTo(mDisconnected);
                     break;
                 case A2dpStackEvent.CONNECTION_STATE_CONNECTED:
-                    if (mA2dpService.okToConnect(mDevice)) {
+                    if (mA2dpService.okToConnect(mDevice, false)) {
                         Log.w(TAG, "Disconnecting interrupted: device is connected: " + mDevice);
                         transitionTo(mConnected);
                     } else {
@@ -438,7 +438,7 @@ final class A2dpStateMachine extends StateMachine {
                     }
                     break;
                 case A2dpStackEvent.CONNECTION_STATE_CONNECTING:
-                    if (mA2dpService.okToConnect(mDevice)) {
+                    if (mA2dpService.okToConnect(mDevice, false)) {
                         Log.i(TAG, "Disconnecting interrupted: try to reconnect: " + mDevice);
                         transitionTo(mConnecting);
                     } else {
