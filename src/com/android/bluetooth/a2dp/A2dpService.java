c885854d5a839b03be8f9a24f50c3b360b1db40d
ValdikSS <iam@valdikss.org.ru>
2018-09-10 02:02:50 +0300

    SBC Dual Channel (SBC HD Audio) support
    "HD Audio" checkbox is not checked for SBC by default.
    
    Change-Id: Id45c48aeaa2eb86f3dffa0cc825d1e09f1c00187

--- src/com/android/bluetooth/a2dp/A2dpService.java
+++ src/com/android/bluetooth/a2dp/A2dpService.java
@@ -68,7 +68,11 @@ public class A2dpService extends ProfileService {
             boolean supportsOptional = false;
             for (BluetoothCodecConfig config :
                     mStateMachine.getCodecStatus().getCodecsSelectableCapabilities()) {
-                if (!config.isMandatoryCodec()) {
+                if (!(config.isMandatoryCodec())
+                        || (config.isMandatoryCodec()
+                                   && (config.getChannelMode() & config.CHANNEL_MODE_DUAL_CHANNEL)
+                                           == config.CHANNEL_MODE_DUAL_CHANNEL)) {
+                    /* SBC Dual Channel capability */
                     supportsOptional = true;
                     break;
                 }
