006364617495fe1df8bd9b2db5f61a726a55ff05
Ugo Yu <ugoyu@google.com>
2018-10-11 19:24:42 +0800

    DO NOT MERGE Separate SDP procedure from bonding state (2/2)
    - Do not stay in bonding state if the device is paried but still
      discovering service.
    - Report BOND_BONDED to Java after authentication is completed.
    - Change bond state to bond none if a classic Bluetooth device
      SDP failed while pairing.
    - Hold BOND_BONDED intent util SDP is findished.
    - Only accept profile connection for the device is at bonded
      state. Any attempt to connect while bonding would potentially
      lead to an unauthorized connection.
    
    Bug: 79703832
    Test: runtest bluetooth, regression test
    Change-Id: Ic43bb63fa5d4f10e08e577483f568f8025f0b1f3
    Merged-In: Ic43bb63fa5d4f10e08e577483f568f8025f0b1f3
    (cherry picked from commit c9c30ef6f8b4f54f66fbfa2c1c0a9826f89b05e2)

--- src/com/android/bluetooth/hid/HidService.java
+++ src/com/android/bluetooth/hid/HidService.java
@@ -655,6 +655,10 @@ public class HidService extends ProfileService {
            (device.getBondState() == BluetoothDevice.BOND_NONE))
             return false;
 
+        if (device.getBondState() != BluetoothDevice.BOND_BONDED) {
+            return false;
+        }
+
         return true;
     }
     private static int convertHalState(int halState) {
