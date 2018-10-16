9428eff36f42f637ee16c0c08d8d41647ade0d20
M Safoorah Banu <safoorah@codeaurora.org>
2018-10-16 10:32:46 +0530

    BT: API to update quiet mode status in Adapter service [1/3]
     - In BLE on use case, quiet mode status is not updated to
       Adapter service which results in not initiating auto
       connection of hfp & a2dp though quiet mode is disabled
       for next turn On.
    
     - Added new API to update quiet mode status in Adapter
       service.
    
    CRs-Fixed: 2337538
    Change-Id: Ifb848e3cc124dd937db09e6d3fbc0996668390ce

--- src/com/android/bluetooth/btservice/AdapterService.java
+++ src/com/android/bluetooth/btservice/AdapterService.java
@@ -1727,6 +1727,16 @@ public class AdapterService extends Service {
         }
 
         @Override
+        public void updateQuietModeStatus(boolean quietMode) {
+            AdapterService service = getService();
+            if (service == null) {
+                return;
+            }
+            service.updateQuietModeStatus(quietMode);
+        }
+
+
+        @Override
         public void onBrEdrDown() {
             AdapterService service = getService();
             if (service == null) {
@@ -2546,6 +2556,12 @@ public class AdapterService extends Service {
     public int getTotalNumOfTrackableAdvertisements() {
         enforceCallingOrSelfPermission(BLUETOOTH_PERM, "Need BLUETOOTH permission");
         return mAdapterProperties.getTotalNumOfTrackableAdvertisements();
+    }
+
+    void updateQuietModeStatus(boolean quietMode) {
+        debugLog("updateQuietModeStatus()-updateQuietModeStatus called with quiet mode status:"
+                   + quietMode);
+        mQuietmode = quietMode;
     }
 
     void onLeServiceUp() {
