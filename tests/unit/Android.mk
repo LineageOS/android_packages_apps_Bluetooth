38e4c2f1433dee229e3276979d53ceded22824ba
Ugo Yu <ugoyu@google.com>
2019-05-07 16:36:19 +0800

    Add unit tests for database migration
     * Generate json schemas of each database version
     * Add tests for database migration on version 100->101 and 101->102.
    
    Bug: 129861166
    Test: atest BluetoothInstrumentationTests
    
    Change-Id: I63f8605862b72ddc2f2bd5c4c07257e6d0419ed0

--- tests/unit/Android.mk
+++ tests/unit/Android.mk
@@ -17,7 +17,14 @@ LOCAL_STATIC_JAVA_LIBRARIES :=  \
     com.android.emailcommon \
     androidx.test.rules \
     mockito-target \
-    androidx.test.espresso.intents
+    androidx.test.espresso.intents \
+    gson-prebuilt-jar \
+    bt-androidx-room-migration-nodeps \
+    bt-androidx-room-runtime-nodeps \
+    bt-androidx-room-testing-nodeps
+
+LOCAL_ASSET_DIR += \
+    $(LOCAL_PATH)/src/com/android/bluetooth/btservice/storage/schemas
 
 # Include all test java files.
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
@@ -30,3 +37,14 @@ LOCAL_COMPATIBILITY_SUITE := device-tests
 LOCAL_INSTRUMENTATION_FOR := Bluetooth
 
 include $(BUILD_PACKAGE)
+
+include $(CLEAR_VARS)
+
+ROOM_LIBS_PATH := ../../lib/room
+
+LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES := \
+    bt-androidx-room-migration-nodeps:$(ROOM_LIBS_PATH)/room-migration-2.0.0-beta01.jar \
+    bt-androidx-room-runtime-nodeps:$(ROOM_LIBS_PATH)/room-runtime-2.0.0-alpha1.aar \
+    bt-androidx-room-testing-nodeps:$(ROOM_LIBS_PATH)/room-testing-2.0.0-alpha1.aar
+
+include $(BUILD_MULTI_PREBUILT)
