b2b4bf5db6285eb23907d8727b92eea314fc7d97
Ugo Yu <ugoyu@google.com>
2019-05-23 20:13:17 +0800

    DO NOT MERGE Separate SDP procedure from bonding state (2/2)
    - Hold BOND_BONDED intent util SDP is findished.
    - Only accept profile connection for the device is at bonded
      state. Any attempt to connect while bonding would potentially
      lead to an unauthorized connection.
    
    Bug: 79703832
    Test: runtest bluetooth, regression test
    Change-Id: Ifefb749e0274ae018714340654287a0412b59b89
    (cherry picked from commit 55d82d63b7ba706eb7fa7f3cfd3e050d1a87a5f7)

--- src/com/android/bluetooth/a2dp/A2dpStateMachine.java
+++ src/com/android/bluetooth/a2dp/A2dpStateMachine.java
@@ -882,7 +882,9 @@ final class A2dpStateMachine extends StateMachine {
         else if((BluetoothProfile.PRIORITY_OFF < priority) ||
                 ((BluetoothProfile.PRIORITY_UNDEFINED == priority) &&
                 (device.getBondState() != BluetoothDevice.BOND_NONE))){
-            ret= true;
+            if (device.getBondState() == BluetoothDevice.BOND_BONDED) {
+                ret = true;
+            }
         }
         return ret;
     }
