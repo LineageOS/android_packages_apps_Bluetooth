6148a905a4c8515d981db59ec892991f7e3096bd
Venkata Jagadeesh Garaga <vgaraga@codeaurora.org>
2019-06-27 11:24:21 +0530

    GAP: Reset bondingInitiatedLocally flag(2/3)
    When Pairing initiated from DUT, bondingIinitiatedLocally
    flag sets from adapterservice. If user disable profile
    options from UI and tries to connect from remote,DUT
    rejects the incoming connection, when dut rejects
    connection some remotes may initiates pairing agiagn.
    
    Profile options should be in disable state after incoming
    pairing successful as user disabled profile options, but
    profile options getting enabled as bondingInitiatedLocally
    not reset.
    
    Hence Reset bondingInitiatedLocally flag after connection
    
     Conflicts:
    	src/com/android/bluetooth/btservice/AdapterService.java
    
    Change-Id: I1c1fd8681f25a9e957b5c91be51b587011701956
    CRs-Fixed: 2457374
    Signed-off-by: Volodymyr Zhdanov <wight554@gmail.com>

--- src/com/android/bluetooth/btservice/AdapterService.java
+++ src/com/android/bluetooth/btservice/AdapterService.java
@@ -1501,6 +1501,17 @@ public class AdapterService extends Service {
         }
 
         @Override
+        public void setBondingInitiatedLocally(BluetoothDevice device, boolean localInitiated) {
+            // don't check caller, may be called from system UI
+            AdapterService service = getService();
+            if (service == null) {
+                return;
+            }
+            service.setBondingInitiatedLocally(device,localInitiated);
+            return;
+        }
+
+        @Override
         public long getSupportedProfiles() {
             AdapterService service = getService();
             if (service == null) {
@@ -2375,6 +2386,17 @@ public class AdapterService extends Service {
             return BluetoothDevice.BOND_NONE;
         }
         return deviceProp.getBondState();
+    }
+
+    void setBondingInitiatedLocally(BluetoothDevice device, boolean localInitiated) {
+        enforceCallingOrSelfPermission(BLUETOOTH_PERM, "Need BLUETOOTH permission");
+        DeviceProperties deviceProp = mRemoteDevices.getDeviceProperties(device);
+        if (deviceProp == null) {
+            return;
+        }
+        Log.w(TAG," localInitiated " + localInitiated);
+        deviceProp.setBondingInitiatedLocally(localInitiated);
+        return;
     }
 
     int getConnectionState(BluetoothDevice device) {
