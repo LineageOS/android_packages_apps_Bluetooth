7b06f67ef951e839632535ce422ed80c62c70166
Zach Johnson <zachoverflow@google.com>
2020-05-22 10:07:30 -0700

    Pipe down init flags to the native stack
    Bug: 156757711
    Test: atest bluetooth_test_gd:InitFlags
    Tag: #gd-refactor
    Change-Id: I338f54d0f8b21ccbb9d7bc350a5d1151e4132469

--- src/com/android/bluetooth/btservice/AdapterService.java
+++ src/com/android/bluetooth/btservice/AdapterService.java
@@ -71,6 +71,7 @@ import android.os.SystemClock;
 import android.os.SystemProperties;
 import android.os.UserHandle;
 import android.os.UserManager;
+import android.provider.DeviceConfig;
 import android.provider.Settings;
 import android.text.TextUtils;
 import android.util.Base64;
@@ -441,7 +442,7 @@ public class AdapterService extends Service {
         mBluetoothKeystoreService = new BluetoothKeystoreService(isNiapMode());
         mBluetoothKeystoreService.start();
         int configCompareResult = mBluetoothKeystoreService.getCompareResult();
-        initNative(isGuest(), isNiapMode(), configCompareResult);
+        initNative(isGuest(), isNiapMode(), configCompareResult, getInitFlags());
         mNativeAvailable = true;
         mCallbacks = new RemoteCallbackList<IBluetoothCallback>();
         mAppOps = getSystemService(AppOpsManager.class);
@@ -3052,6 +3053,15 @@ public class AdapterService extends Service {
                 .isCommonCriteriaModeEnabled(null);
     }
 
+    private static final String GD_CORE_FLAG = "INIT_gd_core";
+    private String[] getInitFlags() {
+        ArrayList<String> initFlags = new ArrayList<>();
+        if (DeviceConfig.getBoolean(DeviceConfig.NAMESPACE_BLUETOOTH, GD_CORE_FLAG, false)) {
+            initFlags.add(GD_CORE_FLAG);
+        }
+        return initFlags.toArray(new String[0]);
+    }
+
     /**
      *  Obfuscate Bluetooth MAC address into a PII free ID string
      *
@@ -3081,8 +3091,8 @@ public class AdapterService extends Service {
 
     static native void classInitNative();
 
-    native boolean initNative(boolean startRestricted, boolean isNiapMode,
-            int configCompareResult);
+    native boolean initNative(boolean startRestricted, boolean isNiapMode, int configCompareResult,
+            String[] initFlags);
 
     native void cleanupNative();
 
