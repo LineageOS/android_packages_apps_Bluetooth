b681de28c14d8effd4edf46e7b356aa2c08e1441
dianlujitao <dianlujitao@lineageos.org>
2020-05-28 22:27:32 +0800

    Stub TWS related APIs
    Change-Id: I653611fd906e7ab6d1fb7a6dbda57bf8e47d6976

--- src/com/android/bluetooth/btservice/AdapterService.java
+++ src/com/android/bluetooth/btservice/AdapterService.java
@@ -1875,6 +1875,14 @@ public class AdapterService extends Service {
             return true;
         }
 
+        public boolean isTwsPlusDevice(BluetoothDevice device) {
+            return false;
+        }
+
+        public String getTwsPlusPeerAddress(BluetoothDevice device) {
+            return null;
+        }
+
         @Override
         public int getBatteryLevel(BluetoothDevice device) {
             AdapterService service = getService();
