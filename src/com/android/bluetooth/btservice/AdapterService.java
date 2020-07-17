ff2243dd2d8356c50a294362ae580bbd8cb85def
Sravan voleti <sravankumar@codeaurora.org>
2020-07-17 14:26:51 +0530

    DeviceGroup: Add Binder method in Bluetooth App
    Override newly added Group specific method to
    Bluetooth app to fix compilation issue.
    
    CRs-Fixed: 2829452
    Change-Id: I83583f219c82275e07c91ac20039c729326ee592

--- src/com/android/bluetooth/btservice/AdapterService.java
+++ src/com/android/bluetooth/btservice/AdapterService.java
@@ -167,6 +167,8 @@ public class AdapterService extends Service {
 
     private final ArrayList<DiscoveringPackage> mDiscoveringPackages = new ArrayList<>();
 
+    private static final int TYPE_BREDR = 100;
+
     static {
         classInitNative();
     }
@@ -1085,6 +1087,10 @@ public class AdapterService extends Service {
             mService.invalidateBluetoothGetStateCache();
             BluetoothAdapter.getDefaultAdapter().disableBluetoothGetStateCache();
         }
+
+        // TODO(b/140404592): Either implement these custom methods, or remove them from IBluetooth.
+        @Override
+        public int getDeviceType(BluetoothDevice device) { return TYPE_BREDR; }
 
         public void cleanup() {
             mService = null;
