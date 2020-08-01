d9164717568164dbc29da494b4124753cca2d7c8
Rahul Sabnis <rahulsabnis@google.com>
2020-07-31 19:47:06 -0700

    Pass whether the local device is an atv device to the native layer to determine whether to include pairing dialogs for justworks and encryption only LE pairing
    Tag: #feature
    Bug: 157038281
    Test: Manual
    Merged-In: Ib7575ff3d5f7e0c208743eff652440f7947dfed7
    Change-Id: Ib7575ff3d5f7e0c208743eff652440f7947dfed7

--- src/com/android/bluetooth/btservice/AdapterService.java
+++ src/com/android/bluetooth/btservice/AdapterService.java
@@ -442,7 +442,11 @@ public class AdapterService extends Service {
         mBluetoothKeystoreService = new BluetoothKeystoreService(isNiapMode());
         mBluetoothKeystoreService.start();
         int configCompareResult = mBluetoothKeystoreService.getCompareResult();
-        initNative(isGuest(), isNiapMode(), configCompareResult, getInitFlags());
+
+        // Android TV doesn't show consent dialogs for just works and encryption only le pairing
+        boolean isAtvDevice = getApplicationContext().getPackageManager().hasSystemFeature(
+                PackageManager.FEATURE_LEANBACK_ONLY);
+        initNative(isGuest(), isNiapMode(), configCompareResult, getInitFlags(), isAtvDevice);
         mNativeAvailable = true;
         mCallbacks = new RemoteCallbackList<IBluetoothCallback>();
         mAppOps = getSystemService(AppOpsManager.class);
@@ -3100,7 +3104,7 @@ public class AdapterService extends Service {
     static native void classInitNative();
 
     native boolean initNative(boolean startRestricted, boolean isNiapMode, int configCompareResult,
-            String[] initFlags);
+            String[] initFlags, boolean isAtvDevice);
 
     native void cleanupNative();
 
