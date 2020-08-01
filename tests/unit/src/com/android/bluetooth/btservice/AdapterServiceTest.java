ee033e18b81fa05bb3fe1907a408deed46b90c01
Rahul Sabnis <rahulsabnis@google.com>
2020-07-31 19:47:06 -0700

    Pass whether the local device is an atv device to the native layer to determine whether to include pairing dialogs for justworks and encryption only LE pairing
    Tag: #feature
    Bug: 157038281
    Test: Manual
    Merged-In: Ib7575ff3d5f7e0c208743eff652440f7947dfed7
    Change-Id: Ib7575ff3d5f7e0c208743eff652440f7947dfed7
    (cherry picked from commit 9cfb9204e573b34da01415f32798cb5376f95555)

--- tests/unit/src/com/android/bluetooth/btservice/AdapterServiceTest.java
+++ tests/unit/src/com/android/bluetooth/btservice/AdapterServiceTest.java
@@ -101,7 +101,8 @@ public class AdapterServiceTest {
         }
         Assert.assertNotNull(Looper.myLooper());
         AdapterService adapterService = new AdapterService();
-        adapterService.initNative(false /* is_restricted */, false /* is_single_user_mode */);
+        adapterService.initNative(false /* is_restricted */, false /* is_single_user_mode */,
+                false);
         adapterService.cleanupNative();
         HashMap<String, HashMap<String, String>> adapterConfig = TestUtils.readAdapterConfig();
         Assert.assertNotNull(adapterConfig);
