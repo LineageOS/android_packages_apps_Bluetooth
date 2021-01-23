3a866bd709525c36a2851cc6ddd5d14a565c0014
pramod kotreshappa <kpramod@codeaurora.org>
2021-01-22 18:21:17 -0800

    Support for broadcast active api
    CRs-fixed: 2856233
    Change-Id: If81f5983c563bd8e4893888f0eaa999050017899

--- src/com/android/bluetooth/btservice/AdapterService.java
+++ src/com/android/bluetooth/btservice/AdapterService.java
@@ -1603,6 +1603,11 @@ public class AdapterService extends Service {
         }
 
         @Override
+        public boolean isBroadcastActive() {
+            return false;
+        }
+
+        @Override
         public int getRemoteType(BluetoothDevice device) {
             AdapterService service = getService();
             if (service == null || !callerIsSystemOrActiveOrManagedUser(service, TAG, "getRemoteType")) {
