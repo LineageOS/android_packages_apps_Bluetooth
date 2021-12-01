ce6cf803a61b746b8db5dedb9dff9cf2a5aac5ed
Martin Brabham <optedoblivion@google.com>
2021-12-01 08:26:26 +0000

    BLE Scanning: Receive the original address to onScanResults
    Bug: 202162086
    Test: Manual
    Tag: #feature
    Change-Id: I56d7c0dfa2fb0622707595ba716d0ce1d19ca33a

--- src/com/android/bluetooth/gatt/GattService.java
+++ src/com/android/bluetooth/gatt/GattService.java
@@ -331,7 +331,8 @@ public class GattService extends ProfileService {
                             }
                             for (String test : TEST_MODE_BEACONS) {
                                 onScanResultInternal(0x1b, 0x1, "DD:34:02:05:5C:4D", 1, 0, 0xff,
-                                        127, -54, 0x0, HexDump.hexStringToByteArray(test));
+                                        127, -54, 0x0, HexDump.hexStringToByteArray(test),
+                                        "DD:34:02:05:5C:4E");
                             }
                             sendEmptyMessageDelayed(0, DateUtils.SECOND_IN_MILLIS);
                         }
@@ -1133,23 +1134,24 @@ public class GattService extends ProfileService {
 
     void onScanResult(int eventType, int addressType, String address, int primaryPhy,
             int secondaryPhy, int advertisingSid, int txPower, int rssi, int periodicAdvInt,
-            byte[] advData) {
+            byte[] advData, String originalAddress) {
         // When in testing mode, ignore all real-world events
         if (isTestModeEnabled()) return;
 
         onScanResultInternal(eventType, addressType, address, primaryPhy, secondaryPhy,
-                advertisingSid, txPower, rssi, periodicAdvInt, advData);
+                advertisingSid, txPower, rssi, periodicAdvInt, advData, originalAddress);
     }
 
     void onScanResultInternal(int eventType, int addressType, String address, int primaryPhy,
             int secondaryPhy, int advertisingSid, int txPower, int rssi, int periodicAdvInt,
-            byte[] advData) {
+            byte[] advData, String originalAddress) {
         if (VDBG || mAdapterService.getIsVerboseLoggingEnabledForAll()) {
             Log.d(TAG, "onScanResult() - eventType=0x" + Integer.toHexString(eventType)
                     + ", addressType=" + addressType + ", address=" + address + ", primaryPhy="
                     + primaryPhy + ", secondaryPhy=" + secondaryPhy + ", advertisingSid=0x"
                     + Integer.toHexString(advertisingSid) + ", txPower=" + txPower + ", rssi="
-                    + rssi + ", periodicAdvInt=0x" + Integer.toHexString(periodicAdvInt));
+                    + rssi + ", periodicAdvInt=0x" + Integer.toHexString(periodicAdvInt)
+                    + ", originalAddress=" + originalAddress);
         }
 
         byte[] legacyAdvData = Arrays.copyOfRange(advData, 0, 62);
