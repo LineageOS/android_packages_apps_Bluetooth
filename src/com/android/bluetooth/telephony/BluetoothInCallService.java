miyu-xu/packages-modules-Bluetooth

BluetoothInCallService: Use handle as the key of mClccIndexMap
Bug: 147613934
Test: atest BluetoothInstrumentationTests
Change-Id: I9542ffca7bf95901e95524a325073978050ca70c

android/app/src/com/android/bluetooth/telephony/BluetoothInCallService.java
@@ -128,7 +128,7 @@ public class BluetoothInCallService extends InCallService {
     public final HashMap<Integer, BluetoothCall> mBluetoothCallHashMap = new HashMap<>();
 
     // A map from Calls to indexes used to identify calls for CLCC (C* List Current Calls).
-    private final Map<BluetoothCall, Integer> mClccIndexMap = new HashMap<>();
+    private final Map<String, Integer> mClccIndexMap = new HashMap<>();
 
     private static BluetoothInCallService sInstance = null;
 
@@ -580,7 +580,7 @@ public class BluetoothInCallService extends InCallService {
             mBluetoothCallHashMap.remove(call.getId());
         }
 
-        mClccIndexMap.remove(call);
+        mClccIndexMap.remove(getClccMapKey(call));
         updateHeadsetWithCallState(false /* force */);
     }
 
@@ -756,13 +756,28 @@ public class BluetoothInCallService extends InCallService {
         }
     }
 
+    private static String getClccMapKey(BluetoothCall call) {
+        if (mCallInfo.isNullCall(call) && call.getHandle() == null) {
+            return "";
+        }
+        Uri handle = call.getHandle();
+        String key;
+        if (call.hasProperty(Call.Details.PROPERTY_SELF_MANAGED)) {
+            key = handle.toString() + " self managed " + call.getId();
+        } else {
+            key = handle.toString();
+        }
+        return key;
+    }
+
     /**
      * Returns the caches index for the specified call.  If no such index exists, then an index is
      * given (smallest number starting from 1 that isn't already taken).
      */
     private int getIndexForCall(BluetoothCall call) {
-        if (mClccIndexMap.containsKey(call)) {
-            return mClccIndexMap.get(call);
+        String key = getClccMapKey(call);
+        if (mClccIndexMap.containsKey(key)) {
+            return mClccIndexMap.get(key);
         }
 
         int i = 1;  // Indexes for bluetooth clcc are 1-based.
@@ -771,7 +786,7 @@ public class BluetoothInCallService extends InCallService {
         }
 
         // NOTE: Indexes are removed in {@link #onCallRemoved}.
-        mClccIndexMap.put(call, i);
+        mClccIndexMap.put(key, i);
         return i;
     }
 

LineageOS/android_packages_apps_Bluetooth

Merged-In: I9542ffca7bf95901e95524a325073978050ca70c
(cherry picked from commit d3d5c639e133ddc210d55f6fb78b30603eb624f5)

src/com/android/bluetooth/telephony/BluetoothInCallService.java
@@ -129,7 +129,7 @@ public class BluetoothInCallService extends InCallService {
     public final HashMap<String, BluetoothCall> mBluetoothCallHashMap = new HashMap<>();
 
     // A map from Calls to indexes used to identify calls for CLCC (C* List Current Calls).
-    private final Map<BluetoothCall, Integer> mClccIndexMap = new HashMap<>();
+    private final Map<String, Integer> mClccIndexMap = new HashMap<>();
 
     private static BluetoothInCallService sInstance = null;
 
@@ -577,7 +577,7 @@ public class BluetoothInCallService extends InCallService {
             mBluetoothCallHashMap.remove(call.getTelecomCallId());
         }
 
-        mClccIndexMap.remove(call);
+        mClccIndexMap.remove(getClccMapKey(call));
         updateHeadsetWithCallState(false /* force */);
     }
 
@@ -753,13 +753,28 @@ public class BluetoothInCallService extends InCallService {
         }
     }
 
+    private String getClccMapKey(BluetoothCall call) {
+        if (mCallInfo.isNullCall(call) || call.getHandle() == null) {
+            return "";
+        }
+        Uri handle = call.getHandle();
+        String key;
+        if (call.hasProperty(Call.Details.PROPERTY_SELF_MANAGED)) {
+            key = handle.toString() + " self managed " + call.getTelecomCallId();
+        } else {
+            key = handle.toString();
+        }
+        return key;
+    }
+
     /**
      * Returns the caches index for the specified call.  If no such index exists, then an index is
      * given (smallest number starting from 1 that isn't already taken).
      */
     private int getIndexForCall(BluetoothCall call) {
-        if (mClccIndexMap.containsKey(call)) {
-            return mClccIndexMap.get(call);
+        String key = getClccMapKey(call);
+        if (mClccIndexMap.containsKey(key)) {
+            return mClccIndexMap.get(key);
         }
 
         int i = 1;  // Indexes for bluetooth clcc are 1-based.
@@ -768,7 +783,7 @@ public class BluetoothInCallService extends InCallService {
         }
 
         // NOTE: Indexes are removed in {@link #onCallRemoved}.
-        mClccIndexMap.put(call, i);
+        mClccIndexMap.put(key, i);
         return i;
     }
 