505a8b584a8cefd5e25bcd7eb77b4ad9f6281d11
Brian Delwiche <delwiche@google.com>
2022-09-28 23:30:49 +0000

    Fix URI check in BluetoothOppUtility.java
    Bug: 225880741
    Test: BT unit tests, validated against researcher POC
    Tag: #security
    Ignore-AOSP-First: Security
    Change-Id: I65c1494023930aa23fede55936488f605c7cfe01
    (cherry picked from commit d0957cfdf1fc1b36620c1545643ffbc37f0ac24c)
    Merged-In: I65c1494023930aa23fede55936488f605c7cfe01

--- src/com/android/bluetooth/opp/BluetoothOppUtility.java
+++ src/com/android/bluetooth/opp/BluetoothOppUtility.java
@@ -46,6 +46,7 @@ import android.database.Cursor;
 import android.net.Uri;
 import android.os.Environment;
 import android.os.SystemProperties;
+import android.util.EventLog;
 import android.util.Log;
 
 import com.android.bluetooth.R;
@@ -75,7 +76,11 @@ public class BluetoothOppUtility {
             new ConcurrentHashMap<Uri, BluetoothOppSendFileInfo>();
 
     public static boolean isBluetoothShareUri(Uri uri) {
-        return uri.toString().startsWith(BluetoothShare.CONTENT_URI.toString());
+        if (uri.toString().startsWith(BluetoothShare.CONTENT_URI.toString())
+                && !uri.getAuthority().equals(BluetoothShare.CONTENT_URI.getAuthority())) {
+            EventLog.writeEvent(0x534e4554, "225880741", -1, "");
+        }
+        return uri.getAuthority().equals(BluetoothShare.CONTENT_URI.getAuthority());
     }
 
     public static BluetoothOppTransferInfo queryRecord(Context context, Uri uri) {
