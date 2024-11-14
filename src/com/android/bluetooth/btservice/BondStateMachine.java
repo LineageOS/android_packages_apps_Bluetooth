9b385d89e04563d0b50a9932ac8afff39cfd1b39
Brian Delwiche <delwiche@google.com>
2024-11-14 00:57:15 +0000

    Reset permissions for not bonded device
    According to the PBAP specification,
    The PSE user shall have to confirm at least the first Phone Book Access
    Profile connection from each new PCE.
    
    According to the MAP specification,
    The MCE and MSE shall be bonded before setting up a Message Access Profile
    connection.
    
    Let's remove the permissions when the device is unbonded.
    
    This is a backport of change ag/30386015 but requires minor changes to
    logic.
    
    Flag: EXEMPT, security fix
    Bug: 289375038
    Bug: 289811388
    Test: atest BluetoothInstrumentationTests
    Ignore-AOSP-First: security fix
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:4b159538ea1a0ead38d76ee087be118355d20acb)
    Merged-In: I78640e9d8bc8ea520f309811cd152106663c0e8a
    Change-Id: I78640e9d8bc8ea520f309811cd152106663c0e8a

--- src/com/android/bluetooth/btservice/BondStateMachine.java
+++ src/com/android/bluetooth/btservice/BondStateMachine.java
@@ -410,6 +410,13 @@ final class BondStateMachine extends StateMachine {
             }
         }
 
+        if (newState == BluetoothDevice.BOND_NONE) {
+            // Remove the permissions for unbonded devices
+            mAdapterService.setMessageAccessPermission(device, BluetoothDevice.ACCESS_UNKNOWN);
+            mAdapterService.setPhonebookAccessPermission(device, BluetoothDevice.ACCESS_UNKNOWN);
+            mAdapterService.setSimAccessPermission(device, BluetoothDevice.ACCESS_UNKNOWN);
+        }
+
         Intent intent = new Intent(BluetoothDevice.ACTION_BOND_STATE_CHANGED);
         intent.putExtra(BluetoothDevice.EXTRA_DEVICE, device);
         intent.putExtra(BluetoothDevice.EXTRA_BOND_STATE, newState);
